fb5c5026e6fe54ef89c1a4c1c30c54dcd8a67f05
orukahairuka <sakuraierika03301109@gmail.com>
2024-05-14 21:22:27 +0900

    Initial Commit

---
+++ Sorehasou/SorehasouApp.swift
@@ -0,0 +1,17 @@
+//
+//  SorehasouApp.swift
+//  Sorehasou
+//
+//  Created by 櫻井絵理香 on 2024/05/14.
+//
+
+import SwiftUI
+
+@main
+struct SorehasouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
